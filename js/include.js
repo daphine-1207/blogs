c30d2b4bedd9099318e8ab734d3fbe2027bd00e6
Daphine Nambafu <daphinenambafu@gmail.com>
2024-03-03 22:10:29 +0300

    daphine's blog-site

---
+++ js/include.js
@@ -0,0 +1,43 @@
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+includeHTML ();
+
+function toggleStar(star) {
+  // Toggle the 'checked' class on the clicked star
+  star.classList.toggle('checked');
+  
+  // Get all the stars after the clicked star
+  let stars = Array.from(star.parentElement.children);
+  let starIndex = stars.indexOf(star);
+  
+  // Uncheck stars after the clicked star
+  for (let i = starIndex + 1; i < stars.length; i++) {
+      stars[i].classList.remove('checked');
+  }
+}
